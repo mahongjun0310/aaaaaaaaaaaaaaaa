6598f59b6f100dd1346fa0f944151dd2248b23e5
mahongjun0310 <1397456062@qq.com>
2018-07-06 11:45:32 +0800

    mei

---
+++ src/a.java
@@ -0,0 +1,19 @@
+public class a {
+    private String a;
+    private int b;
+    private  int c;
+
+    public int getB() {
+        return b;
+    }
+
+    public String getA() {
+        return a;
+
+    }
+
+    public void setA(String a) {
+        this.a = a;
+    }
+
+}
